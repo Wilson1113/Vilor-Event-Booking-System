53c8bd5784960c3c09a522f93d7239dfa0c8e657
Wilson1113 <Wilsonteoshiyang@gmail.com>
2024-10-04 21:47:30 +0800

    Update Fetching function
    - try to fetch data from API

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,19 @@
+<script setup>
+const username = "quiz";
+const password = "vilorfunquiz";
+const encodedCredentials = btoa(`${username}:${password}`);
+
+const res = await useFetch("https://quiz.vilor.com/api/events/listing", {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    Authorization: `Basic ${encodedCredentials}`,
+  },
+});
+console.log(res);
+</script>
+
 <template>
   <div>events page</div>
 </template>
